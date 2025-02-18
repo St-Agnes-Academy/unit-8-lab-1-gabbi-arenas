c000994f2dce2d0bd711163ca29ad63981b44b54
gabbi-arenas <91299469+gabbi-arenas@users.noreply.github.com>
2025-02-18 12:05:33 -0600

    submitted 2/18

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 /**
- * Please complete your intro comment block
+ * Unit 8 Lecture Lab 1
+ * The program creates a standard deck of cards, shuffles them, and prints out the result.
+ * @author Gabbi Arenas
+ * @course AP CSA Block 1
+ * @date February 18, 2025
  */
 public class Main 
 {
